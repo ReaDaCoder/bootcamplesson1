89c66937197b935fc32e823f36fad27e03b5612e
Reabetswe <reabetswemakgabutla0@gmail.com>
2024-04-22 13:35:22 +0200

    added contact info on javascript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let name = "Reabetswe";
 let surname = "Makgabutla";
 let bio = "a Front-End Web Developer";
+let contactInfo = {
+    email : "reabetswemakgabutla0@gmail.com",
+    Phone : "+2772 079 6065",
+    location : "South Africa"
+};
